d56f4e182ebaa4287016c52d6b1732a6f5c59b0d
Malika <shavkatovamalika98@gmail.com>
2022-11-05 18:34:05 +0500

    yangi papka ochildi

---
+++ src/components/Carusel/Carusel.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export const Carusel = () => {
+    return (
+        <div>Carusel</div>
+    )
+}
+export default Carusel
---
+++ src/view/Basket/Basket.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import "./Basket.css"
+export const Basket = () => {
+    return (
+        <div>Basket</div>
+    )
+}
+export default Basket
