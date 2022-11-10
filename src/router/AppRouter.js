d5c1d35b6344b4da6814dc7e9bbda4c32aea93ff
Malika <shavkatovamalika98@gmail.com>
2022-11-10 22:23:47 +0500

    home page 1-qism

--- src/router/AppRouter.js
+++ src/router/AppRouter.js
@@ -1,8 +1,18 @@
-import React from 'react'
-
-export const AppRouter = () => {
+import {Routes,Route} from "react-router-dom"
+import Home from "../view/Home/Home"
+import Products from "../view/Products/Products"
+import Basket from "../view/Basket/Basket"
+import Navbar from "../components/Navbar/Navbar"
+function AppRouter() {
     return (
-        <div>AppRouter</div>
+        <>
+        <Navbar/>
+        <Routes>
+            <Route path="/" element={<Home/>}/>
+            <Route path="/basket" element={<Basket/>}/>
+            <Route path="/products" element={<Products/>}/>
+        </Routes>
+        </>
     )
 }
 export default AppRouter
---
+++ src/view/Home/Home2Carusel.js
@@ -0,0 +1,7 @@
+
+function Home2Carusel() {
+  return (
+    <div>Home2Carusel</div>
+  )
+}
+export default Home2Carusel
